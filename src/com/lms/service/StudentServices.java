9c1b60c1ffb5ee8fcac6ac222782917e01111b16
Yasiru Randika <43146170+YasiruRandika@users.noreply.github.com>
2020-09-06 19:07:59 +0530

    update

---
+++ src/com/lms/service/StudentServices.java
@@ -0,0 +1,25 @@
+package com.lms.service;
+
+
+import java.util.logging.Logger;
+
+import com.lms.model.Student;
+
+/**	/**
+ * Imbulana Liyanage D.S.I.	 * Imbulana Liyanage D.S.I.
+ * IT19134772	 * IT19134772
+ */	 
+
+public interface StudentServices {
+	// Initializing logger
+		public static final Logger logger = Logger.getLogger(UserService.class.getName());
+
+
+		/**
+		 * Create student
+		 * 
+		 * @param student
+		 */
+		public int StudentRegistration(Student student);
+
+}
