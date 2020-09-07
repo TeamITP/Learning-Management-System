a6fa19f82ddb50d32ef9c005b6dcbcbdd9408308
Dell <okandaliy@gmail.com>
2020-09-07 11:31:58 +0530

    update

---
+++ src/com/lms/servlet/fileSubmit.java
@@ -0,0 +1,69 @@
+package com.lms.servlet;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+ 
+
+/**
+ * Servlet implementation class fileSubmit
+ */
+@WebServlet("/fileSubmit")
+public class fileSubmit extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public fileSubmit() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+        if(ServletFileUpload.isMultipartContent(request)){
+            try {
+                List <FileItem> multiparts = new ServletFileUpload(new DiskFileItemFactory()).parseRequest(request);
+                for(FileItem item : multiparts){
+                    if(!item.isFormField()){
+                        String name = new File(item.getName()).getName();
+                        item.write( new File("c:/lms/upload" + File.separator + name));
+                    }
+                }
+               //File uploaded successfully
+               request.setAttribute("gurumessage", "File Uploaded Successfully");
+            } catch (Exception ex) {
+               request.setAttribute("gurumessage", "File Upload Failed due to " + ex);
+            }         		
+        }else{
+
+            request.setAttribute("gurumessage","No File found");
+        }
+
+	}
+
+}
