2a86d9a7f38046e3d2d3782bbc9dd87b965512e9
Tharusha98 <68142354+Tharusha98@users.noreply.github.com>
2020-09-09 12:48:12 +0530

    commit12

---
+++ src/com/lms/model/Recorrection.java
@@ -0,0 +1,24 @@
+package com.lms.model;
+
+public class Recorrection {
+	
+	private String description;
+	private String resultId;
+
+	public String getResultId() {
+		return resultId;
+	}
+
+	public void setResultId(String resultId) {
+		this.resultId = resultId;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
---
+++ src/com/lms/service/RecorrectionServices.java
@@ -0,0 +1,11 @@
+package com.lms.service;
+
+import com.lms.model.Recorrection;
+
+public interface RecorrectionServices {
+	
+	
+	
+	public  int InsertResult(Recorrection recorrection);
+
+}
