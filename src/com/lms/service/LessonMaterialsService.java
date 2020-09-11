d2ebf6603c77c5a4880afba1fa741432be8d549a
Yasiru Randika <43146170+YasiruRandika@users.noreply.github.com>
2020-09-11 19:17:52 +0530

    update

---
+++ src/com/lms/service/LessonMaterialsService.java
@@ -0,0 +1,66 @@
+package com.lms.service;
+
+import java.util.ArrayList;
+import java.util.logging.Logger;
+
+import com.lms.model.ReadingMaterial;
+import com.lms.model.VideoMaterial;
+
+/* 
+ * @author W.G. YASIRU RANDIKA 
+ * IT19131184
+ * 
+ * */
+public interface LessonMaterialsService {
+	// Initializing logger
+		public static final Logger logger = Logger.getLogger(UserService.class.getName());
+
+		/**
+		 * Create Reading Material
+		 * 
+		 * @param ReadingMaterial
+		 */
+		public int createReading(ReadingMaterial readingMaterial);
+		
+		/**
+		 * Insert pdf to reading material
+		 * 
+		 * @param pdf path
+		 */
+		public int insertPDFtoReading(ReadingMaterial readingMaterial);
+		
+		/**
+		 * Get list of reading materials
+		 * 
+		 * @param lessonId
+		 */
+		public ArrayList<ReadingMaterial> getListReadingMaterials(String lessonId);
+		
+		/**
+		 * Delete reading material
+		 * 
+		 * @param materialId
+		 */
+		public int deleteReadingMaterial(String materialId);
+		
+		/**
+		 * Delete video material
+		 * 
+		 * @param materialId
+		 */
+		public int deleteVideo(String materialId);
+		
+		/**
+		 *Insert Video 
+		 * 
+		 * @param Video
+		 */
+		public int insertVideo(VideoMaterial videoMaterial);
+		
+		/**
+		 * Get list of video Materials
+		 * 
+		 * @param lessonId
+		 */
+		public ArrayList<VideoMaterial> getListVideo(String lessonId);
+}
