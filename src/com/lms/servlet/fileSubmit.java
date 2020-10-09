831b0f218e5987b68f574ffc178cb2abb48f6360
Yasiru Randika <43146170+YasiruRandika@users.noreply.github.com>
2020-10-09 10:27:31 +0530

    u

--- src/com/lms/servlet/fileSubmit.java
+++ src/com/lms/servlet/fileSubmit.java
@@ -1,19 +1,11 @@
 package com.lms.servlet;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
-
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.disk.DiskFileItemFactory;
-import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 /**
  * Servlet implementation class fileSubmit
