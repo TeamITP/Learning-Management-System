d9448133fdde2d8185e60af0273276fc31cc3069
Tharusha98 <68142354+Tharusha98@users.noreply.github.com>
2020-10-11 09:12:27 +0530

    Create ExaminationServices.java

---
+++ src/com/lms/servlet/ExaminationServices.java
@@ -0,0 +1,21 @@
+package com.lms.service;
+
+import java.util.ArrayList;
+
+import com.lms.model.Examination;
+
+public interface ExaminationServices {
+	
+	//insert Data
+	public int InsertExamination(Examination examination);
+
+
+   public ArrayList<Examination> getExaminationList(String classroomid);
+
+   
+   public Examination getExamination(String examId);
+
+   
+   public String generateReport(String examId,int max, String root);
+
+}
