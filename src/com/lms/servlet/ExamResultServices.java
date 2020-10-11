903699892f03f69c711aaa8a45aa090dd8aa556d
Tharusha98 <68142354+Tharusha98@users.noreply.github.com>
2020-10-11 09:13:09 +0530

    Create ExamResultServices.java

---
+++ src/com/lms/servlet/ExamResultServices.java
@@ -0,0 +1,33 @@
+package com.lms.service;
+
+import java.util.ArrayList;
+
+import com.lms.model.ExamResult;
+
+public interface ExamResultServices {
+	
+	
+	public  int InsertResult(ExamResult examresult);
+	
+	
+	public ArrayList<ExamResult> getExamResultList(String ExamID);
+	
+	
+	
+	public int UpdateResult(String resultId,int marks);
+	
+	
+	public int DeleteResult(String resultid);
+	
+	
+	public ExamResult getstudentmarks(String studentid,String examid);
+	
+	
+	public ExamResult getResult(String Resultid);
+	
+	public ArrayList<ExamResult> getExamResultListByMarks(String ExamID,int max );
+	
+	
+	
+
+}
